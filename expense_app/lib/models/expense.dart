c3cca56898fe4d4c91213566a1efb83fc6b11f08
sametmete <75644552+sametmete@users.noreply.github.com>
2023-11-23 22:02:20 +0300

    WorkShop_21.11.23

---
+++ expense_app/lib/models/expense.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+
+var uuid = const Uuid();
+
+enum Category { work, travel, food, dress }
+
+const categoryIcons = {
+  Category.travel: Icons.travel_explore,
+  Category.dress: Icons.shop,
+  Category.work: Icons.work,
+  Category.food: Icons.food_bank
+};
+
+class Expense {
+  Expense(
+      {required this.name,
+      required this.price,
+      required this.date,
+      required this.category})
+      : id = uuid.v4();
+  final String id;
+  final String name;
+  final double price;
+  final DateTime date;
+  final Category category;
+  // belirli kategoriler
+  // enum
+}
---
+++ expense_app/lib/pages/main_page.dart
@@ -0,0 +1,48 @@
+import 'package:expense_app/pages/expenses_page.dart';
+import 'package:expense_app/widgets/new_expense.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int refreshNumber = 0;
+
+  ExpensesPage expensesPage = const ExpensesPage(0);
+
+  void setStateAndRefresh(int number) {
+    setState(() {
+      refreshNumber = number;
+      expensesPage = ExpensesPage(refreshNumber);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense App"),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.deepPurpleAccent,
+        actions: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return NewExpense(setStateAndRefresh);
+                },
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: expensesPage,
+    );
+  }
+}
